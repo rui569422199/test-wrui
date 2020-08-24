2dd088cec69e82cbd2bbd69af61dd08f624ce67a
wrui <wrui@bw30.com>
2020-08-24 11:35:55 +0800

    wrui

--- src/test/java/personal/wrui/test/test_wrui/AppTest.java
+++ src/test/java/personal/wrui/test/test_wrui/AppTest.java
@@ -1,4 +1,10 @@
 package personal.wrui.test.test_wrui;
 
+import java.util.regex.Pattern;
+
 public class AppTest {
+	public static void main(String[] args) {
+		Pattern p=Pattern.compile("^2\\d{3}-((1[0-2])|(0[1-9]))$");
+		System.out.println(p.matcher("2010-01").matches());
+	}
 }
